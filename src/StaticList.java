afd55dba510ac76238d103b345abded5a34bca8b
Yuri <yuraosipov2012@gmail.com>
2025-04-16 00:35:41 +0300

    Move StaticList into it's own file && add swap()

---
+++ src/StaticList.java
@@ -0,0 +1,52 @@
+public class StaticList<T> {
+    static final int ARRAY_SIZE = 1024;
+    private Object[] list;
+    private int size;
+
+    StaticList() {
+        list = new Object[StaticList.ARRAY_SIZE];
+        size = 0;
+    }
+
+    T Push(T obj) {
+        list[size] = obj;
+        size++;
+        return obj;
+    }
+
+    int GetLen() {
+        return size;
+    }
+
+    T Get(int i) throws ArrayIndexOutOfBoundsException {
+        if (i < 0) {
+            System.err.printf("Got i == %d when real_len == %d\n", i, size);
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        @SuppressWarnings("unchecked")
+        T ret = (T) list[i];
+        return ret;
+    }
+
+    T Pop(int i) throws ArrayIndexOutOfBoundsException {
+        if (i < 0 || size <= i) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        @SuppressWarnings("unchecked")
+        T tmp = (T) list[i];
+        list[i] = null;
+        return tmp;
+    }
+
+    // Hopefully swap two objects in a list, though I'm not sure if it'll work...
+    void Swap(int index_a, int index_b) {
+        T temp = -1 < index_a && index_a < StaticList.ARRAY_SIZE ? list[index_a] : null;
+        if (temp != null)
+            list[index_a] = -1 < index_b && index_b < StaticList.ARRAY_SIZE ? list[index_a] : null;
+
+        if (-1 < index_b && index_b < StaticList.ARRAY_SIZE)
+            list[index_b] = temp;
+    }
+
+}
