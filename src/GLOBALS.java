cc2e8bd6ef57b5ac8416ab8e46afc613d53693f8
LutzGebelman <yuraosipov2012@gmail.com>
2025-04-19 16:17:34 +0300

    Increase world size

--- src/GLOBALS.java
+++ src/GLOBALS.java
@@ -1,5 +1,5 @@
 public final class GLOBALS {
     static LogLevel LOG_LEVEL = LogLevel.WARN;
     static final int MIN_WORLD_POS = 0;
-    static final int MAX_WORLD_POS = 1024;
+    static final int MAX_WORLD_POS = 8192;
 }
