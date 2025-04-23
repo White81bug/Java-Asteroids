864d866989a4016fc668a1367197b8e5141157c2
YuriTheCockroach <yuraosipov2012@gmail.com>
2025-04-23 19:01:45 +0300

    Move `CameraController` into it's own file

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,10 @@
-
 import static com.raylib.Raylib.*;
 import static com.raylib.Raylib.KeyboardKey.*;
 import static com.raylib.Raylib.MouseButton.*;
 
-import com.raylib.Camera2D;
 import com.raylib.Vector2;
 
 import java.lang.ArrayIndexOutOfBoundsException;
-
-class CameraController {
-    private Vector2 target = new Vector2(0, 0);
-    private float zoom = 2.0f;
-    private boolean followPlayer = true;
-
-    Camera2D camera = new Camera2D();
-
-    void Update(Vector2 targetOriginal) {
-
-        if (isKeyPressed(KEY_C)) {
-            followPlayer = !followPlayer;
-        }
-
-        float wheel = getMouseWheelMove();
-        if (wheel != 0.0f) {
-            zoom += wheel * 0.1f;
-            zoom = Math.max(0.1f, Math.min(zoom, 5.0f));
-        }
-
-        if (isMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
-            if (followPlayer)
-                followPlayer = false;
-            Vector2 delta = getMouseDelta();
-            delta = mUtils.vecMul(delta, -1.0f / zoom);
-            target = mUtils.vecAdd(target, delta);
-        }
-
-        if (followPlayer) {
-            target = new Vector2(targetOriginal.getX(), targetOriginal.getY());
-        }
-
-        camera.setOffset(
-                new Vector2(getScreenWidth() / 2.0f, getScreenHeight() / 2.0f));
-        camera.setTarget(target);
-        camera.setRotation(0);
-        camera.setZoom(zoom);
-    }
-
-    Camera2D getCamera() {
-        return camera;
-    }
-
-    void DrawStatus() {
-        drawText(followPlayer ? "Camera: FOLLOW [C]" : "Camera: FREE [C]", 20,
-                20, 20, GREEN);
-    }
-}
 
 class LogicMaster {
 
