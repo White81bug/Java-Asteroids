7d1dee64b53e81a7e70c9e18e2e067ed8fc66e66
YuriTheCockroach <yuraosipov2012@gmail.com>
2025-04-23 19:19:08 +0300

    Removed the println

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ import java.lang.ArrayIndexOutOfBoundsException;
 public class Main {
     public static void main(String args[]) {
         GLOBALS.DEBUG = System.getenv("DEBUG") != null ? true : false;
-        System.out.println(GLOBALS.DEBUG);
         GameState state = GameState.MENU;
 
         initWindow(1280, 800, "Fuck this shit");
